d7911cc56664deace9d91453cfda0a2bf411481e
HermanLoen <71780070+HermanLoen@users.noreply.github.com>
2021-11-02 11:15:28 +0100

    Initial commit workingBranch

--- QuizApp/Program.cs
+++ QuizApp/Program.cs
@@ -3,7 +3,7 @@
 namespace QuizApp {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello Herman and Camilla");
+            Console.WriteLine("Hello Herman, Camilla, Simen and Hanna");
         }
     }
 }
