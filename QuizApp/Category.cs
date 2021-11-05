c35b88aa116943b271643e651625a9baeea496b5
simenhaga <simen.haga@lyse.net>
2021-11-05 09:55:57 +0100

    Changes to Category.cs and uml.puml

--- QuizApp/Category.cs
+++ QuizApp/Category.cs
@@ -1,7 +1,17 @@
 namespace QuizApp
 {
-    public class Category
-    {
-        
+    public class Category {
+        private long _categoryId;
+        private string _categoryName;
+
+        public long CategoryId {
+            get => _categoryId;
+            set => _categoryId = value;
+        }
+
+        public string CategoryName {
+            get => _categoryName;
+            set => _categoryName = value;
+        }
     }
 }
