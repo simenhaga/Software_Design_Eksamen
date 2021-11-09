32bf688f6c1c9d4bd85357f6dd8ceddfd9409f52
simenhaga <simen.haga@lyse.net>
2021-11-09 13:31:22 +0100

    CreateAnimalQuiz now reads from correct file

--- QuizApp/Program.cs
+++ QuizApp/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using static QuizApp.ReadFromFile;
 
 namespace QuizApp {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello Herman, Camilla, Simen and Hanna");
+            CreateAnimalQuiz();
         }
     }
 }
