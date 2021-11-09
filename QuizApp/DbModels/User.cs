65795e09b64ada8ba23f4fffd5f091646f58bad4
HermanLoen <71780070+HermanLoen@users.noreply.github.com>
2021-11-09 12:25:55 +0100

    changed properties

--- QuizApp/DbModels/User.cs
+++ QuizApp/DbModels/User.cs
@@ -4,18 +4,7 @@ namespace QuizApp
 {
     public class User
     {
-        private long _userId;
-        private string _userName;
-        public long UserId
-        {
-            get => _userId;
-            set => _userId = value;
-        }
-
-        public string UserName
-        {
-            get => _userName;
-            set => _userName = value;
-        }
+        public long UserId { get; set; }
+        public string UserName { get; set; }
     }
 }
