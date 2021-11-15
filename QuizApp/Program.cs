9a7c19470b10bcfbd11504e52305e1879736477a
simenhaga <simen.haga@lyse.net>
2021-11-15 13:33:36 +0100

    Even more cleanup

--- QuizApp/Program.cs
+++ QuizApp/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using static QuizApp.ReadFromFile;
+﻿using static QuizApp.ReadFromFile;
 
 namespace QuizApp {
     class Program {
