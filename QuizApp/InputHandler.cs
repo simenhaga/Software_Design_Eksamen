99efa558c57ed85745870642bb666d6fd6fcccf6
HermanLoen <71780070+HermanLoen@users.noreply.github.com>
2021-11-15 12:11:17 +0100

    refactor

--- QuizApp/InputHandler.cs
+++ QuizApp/InputHandler.cs
@@ -2,54 +2,11 @@ using System;
 
 namespace QuizApp
 {
-    public class InputHandler {   
-        string PlayerChoice { get; set; }
-        private string AcceptInput { get; set; }
-
-
+    public class InputHandler {
         public static string Input()
         {
             Console.Write(">> ");
             return Console.ReadLine();
-        }
-
-        /*public void HandelInput()
-        {
-            while ()
-            { 
-                PlayerChoice = GetUserInput();
-                if (PlayerChoice == "1")
-                {
-                    //show quiz
-                }
-
-                if (PlayerChoice == "2") 
-                {
-                    //show quiz
-                }
-
-                if (PlayerChoice == "3")
-                {
-                    // show quiz    
-                }
-
-                if (PlayerChoice == "4")
-                {
-                    //show quiz
-                }
-
-                if (PlayerChoice == "5")
-                {
-                    //show mixed quiz
-                }
-            }
-        }*/
-
-        public static string ReadLine()
-        {
-            string userInput = Console.ReadLine();
-
-            return userInput;
         }
     }
 }
