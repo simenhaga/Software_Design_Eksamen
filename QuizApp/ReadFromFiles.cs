a56921277ec078095e1d35fc5e011c1ee335ee9d
simenhaga <simen.haga@lyse.net>
2021-11-15 13:01:21 +0100

    cleanup

--- QuizApp/ReadFromFiles.cs
+++ QuizApp/ReadFromFiles.cs
@@ -1,12 +1,6 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using MySqlX.XDevAPI.Common;
-using NUnit.Framework;
-using Ubiety.Dns.Core;
+
 
 namespace QuizApp {
     public class ReadFromFiles {
